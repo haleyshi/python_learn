88c6a66e17e114e135878565b12761a3f283da1b
haleyshi <sgh1982@gmail.com>
2017-03-29 11:22:33 +0800

    django demo

--- django_demo/Treasuregram/main_app/views.py
+++ django_demo/Treasuregram/main_app/views.py
@@ -16,9 +16,4 @@ treasures = [
 ]
 
 def index(request):
-    name = 'Gold Nugget'
-    value = 1000.00
-    context = {'treasure_name': name,
-               'treasure_val': value}
-
-    return render(request, 'index.html', context)
+    return render(request, 'index.html', {'treasures': treasures})
