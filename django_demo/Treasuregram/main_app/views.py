e0b263236a9f1ba0260839160654471570a3b2e5
Haley <haley.shi@ericsson.com>
2017-03-29 11:17:34 +0800

    Django Demo

--- django_demo/Treasuregram/main_app/views.py
+++ django_demo/Treasuregram/main_app/views.py
@@ -2,5 +2,23 @@ from django.shortcuts import render
 
 # Create your views here.
 
+class Treasure:
+    def __init__(self, name, value, material, location):
+        self.name = name
+        self.value = value
+        self.material = material
+        self.location = location
+
+treasures = [
+    Treasure("Gold Nugget", 500.00, "gold", "Curly's Creek, NM"),
+    Treasure("Fool's Glod", 0, "pyrite", "Fool's Falls, CO"),
+    Treasure("Coffee Can", 500.00, "tin", "ACME, CA"),
+]
+
 def index(request):
-    return render(request, 'index.html')
+    name = 'Gold Nugget'
+    value = 1000.00
+    context = {'treasure_name': name,
+               'treasure_val': value}
+
+    return render(request, 'index.html', context)
